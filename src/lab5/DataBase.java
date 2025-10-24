6a4784cae970cbdb65a3756b089d648799021537
Abdelrahman-syam10 <aa6727613@gmail.com>
2025-10-24 12:08:51 +0300

    Update DataBase.java

--- src/lab5/DataBase.java
+++ src/lab5/DataBase.java
@@ -4,17 +4,43 @@
  */
 package lab5;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
  * @author msi
  */
-public interface DataBase {
-    public abstract void ReadFromFile();
+public abstract class DataBase {
+    ArrayList<Student> record =new ArrayList<>();
+    private String fileName;
+
+    public DataBase(String fileName) {
+        this.fileName = fileName;
+    }
+    
+    public void ReadFromFile() throws FileNotFoundException
+    {
+      File f = new File(fileName);
+        Scanner s = new Scanner(f);
+        int n = Integer.parseInt(s.nextLine());
+        while (s.hasNextLine()) {
+            String line = s.nextLine();
+            String[] tokens = line.split(",");
+            int id = Integer.parseInt(tokens[0]);
+            String name = tokens[1];
+            int Age = Integer.parseInt(tokens[2]);
+            String gender=tokens[3];
+            String Dep=tokens[4];
+            double gpa=Double.parseDouble(tokens[5]);
+            record.add(new Student(id,name,Age,gender,Dep,gpa));
+        } 
+
+    }
     public abstract void SaveToFile();
     public abstract boolean contains();
     public abstract ArrayList<Student> getAllstudents();
-    
     
 }
