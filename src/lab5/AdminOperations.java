e5f3cfd0a9b13dba08db11d5d090076ca0268b5a
Abdelrahman-syam10 <aa6727613@gmail.com>
2025-10-25 18:36:36 +0300

    Update AdminOperations.java

--- src/lab5/AdminOperations.java
+++ src/lab5/AdminOperations.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package lab5;
+package lab5.pkg2;
 
 /**
  *
@@ -13,8 +13,12 @@ public abstract class AdminOperations extends DataBase{
     public AdminOperations(String fileName) {
         super(fileName);
     }
-   public Student AddStudent(String Line)
-   {  
+   public boolean  AddStudent(Student s)
+   { 
+       if(!contains(s.getStudentID())){
+     record.add(s);
+     return true;}
+     else return false;
    }
    public Student SearchStudent(int id)
    {
@@ -25,14 +29,26 @@ public abstract class AdminOperations extends DataBase{
      }
      return null;
    }
-   public void DeleteStudent(int id)
+   public boolean DeleteStudent(int id)
    {
-      for(int i=0;i<record.size();i++)
-          if(contains(id))
+      for(int i=0;i<record.size();i++){
+          if(contains(id)){
               record.remove(i);
+      return true;}}
+      return false;
    }
-   public void UpdateStudent()
+   public boolean UpdateStudent(int id, String name, int Age,String Gender, String Dep,double gpa)
    {
+       if(contains( id)){
+       Student s= SearchStudent(id);
+       s.setName(name);
+       s.setAge(Age);
+       s.setGender(Gender);
+       s.setDepartment(Dep);
+       s.setGpa(gpa);
+       return true;
+       }
+       return false;
        
    }
    
