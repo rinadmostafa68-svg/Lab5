e4d65fb21fe58642bc0603897449f84a17f3df1b
Abdelrahman-syam10 <aa6727613@gmail.com>
2025-10-25 18:37:31 +0300

    Update DataBase.java

--- src/lab5/DataBase.java
+++ src/lab5/DataBase.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package lab5;
+package lab5.pkg2;
 
 import java.io.File;
 import java.io.FileNotFoundException;
